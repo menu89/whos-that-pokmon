eedf9048744126d12068f29794c0d44826ca8310
Fatima Abrar <abrarfatima89@gamil.com>
2022-04-04 23:37:15 -0400

    preliminary working for the quiz layout

--- pokemon.js
+++ pokemon.js
@@ -1,3 +1,33 @@
+//pull information from the API
+//pull image and name for one from API
+//pull 3 random names from API
+
+//store name in variable for correct answer
+//store all 4 names in an array
+//array to populate the options section
+
+
+
+//loop for reloading the information related to the image and options
+//run the loop once and store all 20 questions into an secondary array(objects - saving each question as an object) - blur the image of the pokemon
+
+//clicking the button
+//remove the blur from the image - change the class of the object using javascript (class in scss which has animation removing the blur)
+// set an interval ... 
+// will validate the options to see if the answer is correct
+//highlight the correct one in green ...
+// if selected the wrong one ... highlight in red
+//variable for keeping score
+//set interval
+//next question - counter that keeps track of how many times it's clicked
+//updates inner text to the next index in the questions array
+
+//place a 20 second limit on each question - active the sequence that would have happened if next question had been selected?
+
+//get the objects from the main page? the form to populate the inner text
+
+//diving deeper (if time permits)
+//every time the user clicks submit, there is a bar on the side, which takes the image of the question, the correct answer and the answer that you chose
 
 
 
